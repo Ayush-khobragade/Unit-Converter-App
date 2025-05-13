9d55173772c0c415a1be33931e6bb95a6fa0ad9a
Ayush-khobragade <ayushk4236@gmail.com>
2025-05-14 00:20:25 +0530

    Added a toast

--- app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.example.unitconverter
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.unitconverter.ui.theme.UnitConverterTheme
 
@@ -35,29 +39,21 @@ class MainActivity : ComponentActivity() {
 fun UnitConverter(){
     Column {
 //        This Fun prints text one by one
-//        Greeting("Android")
-//        Greeting("Android")
-    }
-    Row {
+        Text("Unit Converter")
+        OutlinedTextField(value = "", onValueChange = { })
+        Row {
 //        This fun prints text next to each other
-        Greeting("Android")
-        Greeting("Android")
-        Greeting("Android")
+            val context = LocalContext.current
+            Button(onClick = { Toast.makeText(context,"Thanks For Clicking!",Toast.LENGTH_LONG).show()}) {
+                Text("MyButton")
+            }
+        }
+        Text("Result")
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    UnitConverterTheme {
-        Greeting("Android")
-    }
+fun UnitConverterPreview(){
+    UnitConverter()
 }
